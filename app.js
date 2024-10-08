177d7b6d3356915c967d9de14bef16a5744e775c
Aziz-BenLamine <azizbenlamine@gmail.com>
2024-10-08 20:53:13 +0100

    Improve UI

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require("express");
-const path = require("node:path");
-const indexRouter = require("./routes/index");
-//const userController = require("./controllers/userController");
+const path = require("path");
 const app = express();
 
-//Enable ejs
+// Enable ejs
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-//
+// Middleware to parse URL-encoded data
 app.use(express.urlencoded({ extended: true }));
 
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, "public")));
+
 // Use Routes
-//app.use("/user", userController.getMessages);
+const indexRouter = require("./routes/index"); // Ensure this is correctly required
 app.use("/", indexRouter);
+
 const PORT = 3000;
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Express app listening to Port ${PORT}`);

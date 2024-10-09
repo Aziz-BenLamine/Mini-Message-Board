d2373778b0cd681bf20a096566698871e4a49c16
Aziz-BenLamine <azizbenlamine@gmail.com>
2024-10-09 09:46:03 +0100

    Fix dev/prod env config

--- db/pool.js
+++ db/pool.js
@@ -2,8 +2,10 @@ const { Pool } = require("pg");
 const dotenv = require("dotenv");
 
 dotenv.config();
-
-const connectionString = process.env.DATABASE_PUBLIC_URL;
+const isProduction = process.env.NODE_ENV === "production";
+const connectionString = isProduction
+  ? process.env.DATABASE_URL
+  : process.env.DATABASE_PUBLIC_URL;
 
 module.exports = new Pool({
   connectionString,
